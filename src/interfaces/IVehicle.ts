658675bb41938d81e284130bb56bd824db91a295
bissixp <fernandosbissi18@hotmail.com>
2022-09-15 14:02:31 -0300

    Req 2 feito

---
+++ src/interfaces/IVehicle.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+const VehicleZodShema = z.object({
+  model: z.string().min(3),
+  year: z.number().nonnegative().gte(1900).lte(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number().nonnegative(),
+});
+
+export type IVehicle = z.infer<typeof VehicleZodShema>;
+export { VehicleZodShema };
