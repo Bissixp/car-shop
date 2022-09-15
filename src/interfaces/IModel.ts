715dcc7eb9ea7fa96d7e525509b7f174b3e77fa9
bissixp <fernandosbissi18@hotmail.com>
2022-09-15 12:23:00 -0300

    Req 1 feito

--- src/interfaces/IModel.ts
+++ src/interfaces/IModel.ts
@@ -1,1 +1,7 @@
-console.log('go!');
+export interface IModel<T> {
+  create(obj: T): Promise<T>,
+  read(_id: string): Promise<T[]>,
+  readOne(_id: string): Promise<T | null>,
+  update(_id: string, payload: T): Promise<T | null>,
+  delete(_id: string): Promise<T | null>,
+}
